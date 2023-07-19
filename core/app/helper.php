8f44c64a9bd30eaae1132e7e65b12d618463b98d
mrorko840 <mrorko840@gmail.com>
2023-07-19 20:55:19 +0600

    f

--- core/app/helper.php
+++ core/app/helper.php
@@ -9,7 +9,8 @@ function showDate($d){
 
 function makeQr($id=null){
     $long_text = base64_encode($id."?"."lol=N2Y4MTU3NWI0YTkxMmFhY2FkYTFkMDRiNjQzN2NiNGQ2MmNlYmZkYWYwNzJjZmZjNWYyZTRhMTZjYmFkODI0NmEcgq6cuGWmw1iaQf2aqRuQvjegeC5fJNpTRv6-2lzdZjaydG8VLnI-9JyGj0ZCE2pY-CL");
-    $url = route('card.check.verify', $long_text);
+    $Ltext = str_replace('/','',$long_text);
+    $url = route('card.check.verify', $Ltext);
     // $qrCode = 'https://qrcode.tec-it.com/API/QRCode?data='.$url;
     $qrCode = 'https://chart.googleapis.com/chart?cht=qr&chl='.$url.'&chs=355x355&choe=UTF-8&chld=L|2';
     return $qrCode;
